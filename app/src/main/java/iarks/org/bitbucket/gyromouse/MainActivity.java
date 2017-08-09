73ae7fcbae61a9d134380996f2a226583a044a0a
Arkadeep <arkadeepsaha11@gmail.com>
2017-08-09 15:30:53 +0530

    Removed navigation drawer

--- app/src/main/java/iarks/org/bitbucket/gyromouse/MainActivity.java
+++ app/src/main/java/iarks/org/bitbucket/gyromouse/MainActivity.java
@@ -9,10 +9,6 @@ import android.util.Log;
 import android.view.KeyEvent;
 import android.view.MotionEvent;
 import android.view.View;
-import android.support.design.widget.NavigationView;
-import android.support.v4.view.GravityCompat;
-import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
@@ -27,7 +23,7 @@ import java.util.concurrent.LinkedBlockingDeque;
 
 import static android.view.KeyEvent.KEYCODE_BACK;
 
-public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener
+public class MainActivity extends AppCompatActivity
 {
     Button buttonRight,buttonEscape,buttonLeft,buttonWindows;
     ImageButton buttonAR,buttonAL,buttonAU,buttonAD,buttonMouse,buttonKeyboard,buttonScroll;
@@ -356,28 +352,6 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
                 imm.toggleSoftInput(InputMethodManager.SHOW_IMPLICIT,0);
             }
         });
-
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        drawer.addDrawerListener(toggle);
-        toggle.syncState();
-
-        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
-        navigationView.setNavigationItemSelectedListener(this);
-    }
-
-    @Override
-    public void onBackPressed()
-    {
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        if (drawer.isDrawerOpen(GravityCompat.START))
-        {
-            drawer.closeDrawer(GravityCompat.START);
-        }
-        else
-        {
-            super.onBackPressed();
-        }
     }
 
     @Override
@@ -401,39 +375,12 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
             startActivity(new Intent(android.provider.Settings.ACTION_WIFI_SETTINGS));
             return true;
         }
-
-        return super.onOptionsItemSelected(item);
-    }
-
-    @SuppressWarnings("StatementWithEmptyBody")
-    @Override
-    public boolean onNavigationItemSelected(MenuItem item)
-    {
-        // Handle navigation view item clicks here.
-        int id = item.getItemId();
-
-        if (id == R.id.nav_settings)
+        else if(id == R.id.action_settings)
         {
-            // Handle the camera action
             Intent i = new Intent(this, PreferencesActivity.class);
             startActivity(i);
         }
-//          else if (id == R.id.nav_gallery) {
-//
-//        } else if (id == R.id.nav_slideshow) {
-//
-//        } else if (id == R.id.nav_manage) {
-//
-//        } else if (id == R.id.nav_share) {
-//
-//        } else if (id == R.id.nav_send) {
-//
-//        }
-
-
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        drawer.closeDrawer(GravityCompat.START);
-        return true;
+        return super.onOptionsItemSelected(item);
     }
 
     @Override
