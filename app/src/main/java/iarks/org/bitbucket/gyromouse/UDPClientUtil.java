83d966083651289d15a8cd8d0ade3d6fe370f29c
Arkadeep <arkadeepsaha11@gmail.com>
2017-09-29 18:19:46 +0530

    improve logging in udp client util

--- app/src/main/java/iarks/org/bitbucket/gyromouse/UDPClientUtil.java
+++ app/src/main/java/iarks/org/bitbucket/gyromouse/UDPClientUtil.java
@@ -3,6 +3,7 @@ package iarks.org.bitbucket.gyromouse;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
+import android.util.Log;
 
 import java.io.UnsupportedEncodingException;
 import java.net.DatagramPacket;
@@ -13,42 +14,48 @@ import java.util.concurrent.BlockingQueue;
 
 class UDPClientUtil implements Runnable
 {
-    private static final String TAG = MainActivity.class.getName();
+    private static final String TAG = UDPClientUtil.class.getName();
     private DatagramSocket clientSocket;
     private InetAddress IPAddress;
     private int port;
     private byte[] data;
     private final BlockingQueue<String> sharedQueue;
     private Context contextt;
+    private SharedPreferences prefs;
 
-    UDPClientUtil(BlockingQueue<String> bq, Context context)
+    UDPClientUtil(BlockingQueue<String> blockingQueue, Context context)
     {
         try
         {
+            Log.i(TAG, " in construnctor. Creating socket on this client");
             clientSocket = new DatagramSocket();
-//            Session.datagramSocket=clientSocket;
             contextt=context;
+            Log.i(TAG, " in construnctor. Creating socket on this client complete");
         }
         catch (Exception e)
         {
+            Log.i(TAG,"exception occured when creating socket. aborting thread");
             e.printStackTrace();
+            return;
         }
         finally
         {
-            sharedQueue = bq;
+            sharedQueue = blockingQueue;
         }
     }
 
     void udpSetup()
     {
+        Log.i(TAG, "setting up the udp object instance");
         try
         {
-            SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(contextt);
+            prefs = PreferenceManager.getDefaultSharedPreferences(contextt);
             IPAddress = InetAddress.getByName(Session.getSessionInstance().getServerIP());
             port = Integer.parseInt(prefs.getString("udpPort","9050"));
         }
         catch (UnknownHostException e)
         {
+            Log.i(TAG, "unknown host exception occurred");
             e.printStackTrace();
         }
     }
@@ -61,39 +68,49 @@ class UDPClientUtil implements Runnable
     @Override
     public void run()
     {
+        Log.i(TAG, "thread is running");
         while (true)
         {
             synchronized (sharedQueue)
             {
                 if (!sharedQueue.isEmpty())
                 {
+                    //Log.i(TAG, "data present in queue");
                     try
                     {
                         data = sharedQueue.remove().getBytes("UTF-8");
+                        //Log.i(TAG, "obtained data from queue");
                     }
                     catch (UnsupportedEncodingException e)
                     {
+                        //Log.i(TAG, "encoding not supported");
                         e.printStackTrace();
                     }
                     try
                     {
+                        //Log.i(TAG, "sending command to server");
                         DatagramPacket clientRequestPacket = new DatagramPacket(data, data.length, IPAddress, port);
                         clientSocket.send(clientRequestPacket);
                         data=null;
+                        //Log.i(TAG, "sending command to server complete");
                     }
                     catch (Exception e)
                     {
+                        //Log.i(TAG, "exception occured. unable to send this data");
                         e.printStackTrace();
                     }
                 }
                 else
                 {
+                    //Log.i(TAG, "no data in queue");
                     try
                     {
+                        //Log.i(TAG, "waiting on queue");
                         sharedQueue.wait();
                     }
                     catch (InterruptedException e)
                     {
+                        //Log.i(TAG, "waiting on queue was interrupted");
                         e.printStackTrace();
                     }
                 }
